22ab031d69d8c6c5b934590327fb16279506faf1
Colin Turner <colinturner34@gmail.com>
2019-09-27 17:59:42 -0700

    Updates

---
+++ precache-manifest.928ec438abcf344539cf9e94a89ec8a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6489329afee09b20092fb66c3e010c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91e6f89d7692040866de",
+    "url": "/static/css/main.b4e59d82.chunk.css"
+  },
+  {
+    "revision": "12f24694a78cdd9d22fc",
+    "url": "/static/js/2.cb8107c1.chunk.js"
+  },
+  {
+    "revision": "91e6f89d7692040866de",
+    "url": "/static/js/main.16af7b65.chunk.js"
+  },
+  {
+    "revision": "4db211ee10cdd78c9c22",
+    "url": "/static/js/runtime-main.ce2b5e4e.js"
+  }
+]);
